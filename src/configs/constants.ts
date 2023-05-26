ab7066bcdf7644b21c2dd844f83e0651658f2ba4
Oryngali <oryngali96@gmail.com>
2023-05-26 22:51:40 +0500

    fix: add navbar showcase logic and add initial query for better user experience

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -14,5 +14,9 @@ const githubAuthors: RegularObject = {
 };
 
 const GRAPHQL_API = 'https://rickandmortyapi.com/graphql';
+const queryMock = {
+  query: `query ($id: ID!) { \n\tcharacter(id: $id) { \n\t\tid \n\t\tname \n\t\tstatus \n\t\tspecies \n\t\ttype \n\t\tgender \n\t\torigin { \n\t\t\tid \n\t\t\tname \n\t\t} \n\t\tlocation { \n\t\t\tid \n\t\t\tname \n\t\t} \n\t\timage \n\t\tepisode { \n\t\t\tid \n\t\t\tname \n\t\t\tcreated \n\t\t} \n\t\tcreated \n\t\t} \n}`,
+  variables: `{ \n\t"id": 1 \n}`,
+};
 
-export { Language, LOCALIZATION_COOKIE_KEY, GRAPHQL_API, githubAuthors };
+export { Language, LOCALIZATION_COOKIE_KEY, GRAPHQL_API, githubAuthors, queryMock };
